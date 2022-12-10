dc5f8394af90026be89d2d44be55d70d30f03592
ajengrf12 <ajengrf12@gmail.com>
2022-12-10 21:46:39 +0700

    adjust HomePage

--- src/scenes/MainScene/pages/HomePage/index.js
+++ src/scenes/MainScene/pages/HomePage/index.js
@@ -1,13 +1,21 @@
-import {View} from 'react-native';
+import {FlatList, Image, SafeAreaView, View} from 'react-native';
 import React from 'react';
 import styles from './styles';
 import Text from '../../../../components/Text';
 import {Color} from '../../../../themes';
+import {Icons, Images} from '../../../../assets';
+import Octicons from 'react-native-vector-icons/Octicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Header from './components/Header';
+import ListPoster from './components/ListPoster';
 
 const HomePage = () => {
   return (
     <View style={styles.container}>
-      <Text color={Color.white}>Recently Added</Text>
+      <SafeAreaView style={styles.safeAreaView} />
+      <Header />
+      <ListPoster title="Recently Added" />
+      <ListPoster title="Trending Now" />
     </View>
   );
 };
--- src/scenes/MainScene/pages/HomePage/styles.js
+++ src/scenes/MainScene/pages/HomePage/styles.js
@@ -1,9 +1,12 @@
 import {StyleSheet} from 'react-native';
+import {Color} from '../../../../themes';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
+  },
+  safeAreaView: {
+    backgroundColor: Color.white,
   },
 });
 
