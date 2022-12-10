7fec35a86390ff612075a53532263907a83bf1c2
ajengrf12 <ajengrf12@gmail.com>
2022-12-10 23:02:31 +0700

    number icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,8 @@
 export default {
   netflixLogo: require('./netflix-logo.png'),
+  numberOne: require('./number-one.png'),
+  numberTwo: require('./number-two.png'),
+  numberThree: require('./number-three.png'),
+  numberFour: require('./number-four.png'),
+  numberFive: require('./number-five.png'),
 };
